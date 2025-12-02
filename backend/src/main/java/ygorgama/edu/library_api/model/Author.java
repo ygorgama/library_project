5dd503e95bb6d88899bf494074fb78febefd0d2d
Ygor Gama <ygor.bio.2002@gmail.com>
2025-12-02 14:25:08 -0300

    feature: Author model

---
+++ backend/src/main/java/ygorgama/edu/library_api/model/Author.java
@@ -0,0 +1,43 @@
+package ygorgama.edu.library_api.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 60)
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
